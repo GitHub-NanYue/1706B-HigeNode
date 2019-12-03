3835014e84247ca74203fa00bc9a927689a32141
NY adsAlsgit config --global user.name NY <973651077@qq.com>
2019-12-03 20:38:58 +0800

    作业

---
+++ zuoye1/dome/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+*{
+  padding: 0;
+  margin: 0;
+}
+    // &.router-link-exact-active {
+    //   color: #42b983;
+    // }
+</style>
---
+++ zuoye1/node/app.js
@@ -0,0 +1,26 @@
+let express = require('express')
+let fs = require('fs')
+let app = express()
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+let list = require('./mock/data.json')
+app.get('/data/list',(req,res)=>{
+    res.send(list)
+    
+})
+app.get('/data/search',(req,res)=>{
+    console.log(req)
+    let data = JSON.parse(fs.readFileSync('./mock/data.json','utf8'))
+    console.log(req.query)
+    let result = list.filter((item,index)=>{
+        return item.num.includes(req.query.value) || item.name.includes(req.query.value)
+    })
+    res.send(result)
+})
+
+
+app.listen(8080,()=>{
+    console.log('成功')
+})
